2f9a4b4929e58b0519550b20414a7ca76460355b
Mario Guzman <marioguzman140@gmail.com>
2026-01-18 21:35:54 -0400

    Creado la factura para el tema de compra

--- resources/js/Pages/Purchase/TablePurchase.vue
+++ resources/js/Pages/Purchase/TablePurchase.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
 import AppLayout from "@layout/AppLayout.vue";
 import {ref} from "vue";
-import {productFullI, productI} from "@/Interfaces/ProductInterface";
-import {useRoute} from "ziggy-js";
 import {DataTable, Column, Dialog} from "primevue";
 
 
-
-const route = useRoute();
 /*
 Propiedades
  */
@@ -22,45 +18,6 @@ const showProduct = ref<boolean>(false)
 
 
 
-
-/*
-funciones
- */
-const getData = (item:productFullI) => {
-    //Obtener los datos de productos
-    // let info:purchaseInfoI | undefined = form.info.find((el) => el.name === item.uuid);
-    //
-    // Verificar si el producto exite
-    // if (info?.uuid === item.id)
-    // {
-    //     info.quantity += 1.00;
-    //     showProduct.value = false;
-    //
-    // }else{
-    //
-    //     //Pasar los datos al formulario
-    //     form.info.push({
-    //         uuid:"",
-    //         amount: 0,
-    //         discount: item.discount,
-    //         quantity: 1,
-    //         price: item.price,
-    //         name: item.name,
-    //         tax: item.tax,
-    //         tax_rate: item.tax_rate / 100,
-    //         status: 1
-    //     });
-    //
-    //     //Cerrar la ventana
-    //     showProduct.value = false;
-    // }
-
-    // //Conseguir el index para poder realizar el cálculo
-    // let index = form.info.findIndex((el) => el.uuid === item.uuid);
-
-    //Calcular el indice
-    // totalAmount(index);
-}
 
 </script>
 
