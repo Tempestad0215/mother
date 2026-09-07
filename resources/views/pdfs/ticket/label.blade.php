448b5c2536bfb456607fa1a3912bf64e8fc59013
Mario Guzman <marioguzman140@gmail.com>
2026-09-07 16:24:28 -0400

    Etiqueta de producto con nombre

--- resources/views/pdfs/ticket/label.blade.php
+++ resources/views/pdfs/ticket/label.blade.php
@@ -23,19 +23,22 @@
             text-align: center;
         }
         .app-name {
-            font-size: 14px;
+            font-size: 12px;
             font-weight: 600;
-            letter-spacing: 2px;
+            letter-spacing: 1px;
             text-transform: uppercase;
             color: #333333;
+            margin: 0;
         }
         .barcode-wrapper {
+            margin: 0 5px;
             width: auto;
         }
         .code-text {
-            font-size: 24px;
+            font-size: 10px;
             font-weight: bold;
             letter-spacing: 2px;
+            margin: 0;
             color: #1e293b;
         }
     </style>
@@ -47,13 +50,13 @@
 
     <!-- SVG del código de barras -->
     <div class="barcode-wrapper">
-        {!! \Milon\Barcode\Facades\DNS1DFacade::getBarcodeSVG($code, 'C128', 1.1, 80, 'black', false) !!}
+        {!! \Milon\Barcode\Facades\DNS1DFacade::getBarcodeSVG($code, 'C128', .8, 40) !!}
     </div>
 
     <!-- Texto inferior con el código aumentado y estilizado -->
-    <span class="code-text">
-            {{ $code }}
-        </span>
+{{--    <span class="code-text">--}}
+{{--            {{ $code }}--}}
+{{--        </span>--}}
 </div>
 </body>
 </html>
